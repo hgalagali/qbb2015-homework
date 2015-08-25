273a1a262d22143e2d3edac8b00baa9bcba86598
Himani Galagali <hgalaga1@jhu.edu>
2015-08-25 11:35:40 -0400

    count gene names

--- day2/filter.py
+++ day2/filter.py
@@ -3,12 +3,17 @@
 filename = "/Users/cmdb/qbb2015/stringtie/SRR072893/t_data.ctab"
 f=open(filename)
 
+name_counts = {}
+
 #Iterates the file by line number
 for line_count, line in enumerate(f):
-    if line_count <= 10:
-        pass
-    elif line_count<=15:
-        print line,
-    else:
-        break
+   fields=line.split()
+   gene_name = fields[9]
+   if gene_name not in name_counts:
+       name_counts[gene_name]=1
+   else:
+       name_counts[gene_name]+=1
+        
+for key, value in name_counts.iteritems():
+    print key, value
    
